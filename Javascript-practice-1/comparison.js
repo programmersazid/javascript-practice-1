804851134ed2de69d762c8a8ce7fc39db71a20a6
modabbir <modabbir276@gmail.com>
2024-09-06 23:05:39 +0600

    my Javascript Coding

---
+++ Javascript-practice-1/comparison.js
@@ -0,0 +1,12 @@
+console.log(10 == 10);
+console.log(10 == '10');// double equal sudhu value ke tulona kore
+console.log(10 === '10');// triple equal type data check kore value check korena sudhu 
+console.log(10 != 10);
+console.log(10 !== '10');// !==........aita value check korar sathe type o check kore
+console.log(10 != '10');//!=............aita sudu value check kore
+console.log(10 <= 15);
+console.log(100 <= 15);
+
+var applePrice = 250;
+var orangePrice = 300;
+console.log(applePrice > orangePrice);
