b202d781083d18aab7a6a1885639994807f25035
modabbir <modabbir276@gmail.com>
2024-09-06 23:45:28 +0600

    if-else code

--- Javascript-practice-1/comparison.js
+++ Javascript-practice-1/comparison.js
@@ -10,3 +10,16 @@ console.log(100 <= 15);
 var applePrice = 250;
 var orangePrice = 300;
 console.log(applePrice > orangePrice);
+ if(10<20){
+    console.log("I have a smaller value");
+ }
+ var weight = 40;
+ if(weight < 20)
+ {
+    console.log('I will carry it by myself');
+
+ }
+ else{
+    console.log('I will rent a rickshaw');
+ }
+    
