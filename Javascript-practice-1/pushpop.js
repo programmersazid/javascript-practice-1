b64e32789918559cff7ea547945baaa7d56d54c2
modabbir <modabbir276@gmail.com>
2024-09-11 00:07:20 +0600

    array practice  more

---
+++ Javascript-practice-1/pushpop.js
@@ -0,0 +1,13 @@
+let num = [1,2];
+num.push(3,4);
+console.log(num);
+let out1 = num.shift();
+console.log(out1);
+num.unshift(0);
+console.log(num);
+
+let string = ["Sazid", "Alpona"];
+string.push("Himel");
+console.log(string);
+string.unshift("Hiyan");
+console.log(string);
